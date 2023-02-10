b9bc25ba7ef06d77e5af65967fdff79e2b9e877d
Rani Meliyana Putri <ranimeliyanaputri@gmail.com>
2023-02-10 22:25:25 +0800

    add assets & config tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,21 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero-image': "url('/public/assets/bg-real.svg')",
+      },
+      height: {
+        jumbotron: '36rem',
+      },
+      colors: {
+        'pink-100': '#FFE1E1',
+        'pink-200': '#FF9D9D',
+        'pink-300': '#FF8B8B',
+        'purple-200': '#CA69F8',
+        'black-100': '#4A4A4A',
+      },
+    },
   },
   plugins: [],
 }
